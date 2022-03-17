5d12732347171010ebd4bd23a77fe9767330e816
aaplnv <56654805+aaplnv@users.noreply.github.com>
2022-03-17 17:36:32 +0300

    [NEW] My first steps with SQLite and SQLAlchemy

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# ______ ______              _____
-# ___  //_/__(_)________________(_)_____________ _____________
-# __  ,<  __  /__  ___/_  ___/_  /__  __ \_  __ `/  _ \_  ___/
-# _  /| | _  / _(__  )_(__  )_  / _  / / /  /_/ //  __/  /
-# /_/ |_| /_/  /____/ /____/ /_/  /_/ /_/_\__, / \___//_/
-#                                        /____/
-
 import logging
 import yaml
+import sqlalchemy as db
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -20,10 +14,22 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config['TOKEN'])
 dp = Dispatcher(bot)
 
+# Initialize database
+engine = db.create_engine('sqlite:///kissinger.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+users = db.Table('users', metadata, autoload=True, autoload_with=engine)
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hello world!")
+    user = connection.execute(db.select([users]).where(users.columns.tid == message.from_user.id)).fetchall()
+
+    if len(user) == 0:
+        connection.execute(db.insert(users).values(tid=message.from_user.id))
+        await message.reply("You're a newbie here.. so, new entry inserted!")
+    else:
+        await message.reply("Hi, username! I can remember you. I am watching you! ")
 
 
 if __name__ == '__main__':
