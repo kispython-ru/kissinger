13415c5a277f05fb32a644a654570fb4e183989a
aaplnv <56654805+aaplnv@users.noreply.github.com>
2022-03-17 14:59:51 +0300

    Say hello to the wonderful telegram world!

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world!")
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = ''
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+print("start")
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hello world!")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
