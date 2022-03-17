d5d6238993ea86f852886b4797690416ea82d097
aaplnv <56654805+aaplnv@users.noreply.github.com>
2022-03-17 16:29:49 +0300

    Nothing interesting in this commit

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
+# ______ ______              _____
+# ___  //_/__(_)________________(_)_____________ _____________
+# __  ,<  __  /__  ___/_  ___/_  /__  __ \_  __ `/  _ \_  ___/
+# _  /| | _  / _(__  )_(__  )_  / _  / / /  /_/ //  __/  /
+# /_/ |_| /_/  /____/ /____/ /_/  /_/ /_/_\__, / \___//_/
+#                                        /____/
+
 import logging
 import yaml
 
 from aiogram import Bot, Dispatcher, executor, types
 
+# Load config file
 config = yaml.safe_load(open("config.yml"))
-
-API_TOKEN = config['TOKEN']
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=config['TOKEN'])
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
     await message.reply("Hello world!")
 
 
