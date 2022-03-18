7400321d96c20db67857ad297e9042968efe9274
aaplnv <56654805+aaplnv@users.noreply.github.com>
2022-03-19 00:36:54 +0300

    Now onboarding moved from main too

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import sqlalchemy as db
 from aiogram import Bot, Dispatcher, executor, types
 
 # Load config file
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import Session, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 import dbmanager
+import onboarding
 from models import User
 
 config = yaml.safe_load(open("config.yml"))
@@ -46,7 +45,8 @@ async def send_welcome(message: types.Message):
     #     await message.reply("You're a newbie here.. so, new entry inserted!")
     #    return
 
-    await onboard_select_prefix(message.from_user.id)
+    # TODO: Reimagine this shit
+    await onboarding.select_prefix(message.from_user.id)
 
     await message.reply("Hi, username! I can remember you. I am watching you!")
 
@@ -60,15 +60,15 @@ async def callback_handler(callback: types.CallbackQuery):
         action = payload[0]
 
         if action == "grouponboard":
-            await onboard_select_group(tid=callback.from_user.id, prefix=payload[1], mid=callback.message.message_id)
+            await onboarding.select_group(tid=callback.from_user.id, prefix=payload[1], mid=callback.message.message_id)
             return
         if action == "prefixonboard":
-            await onboard_select_prefix(callback.from_user.id, callback.message.message_id)
+            await onboarding.select_prefix(callback.from_user.id, callback.message.message_id)
             return
         if action == "variantonboard":
             if len(payload) > 1:
                 await dbmanager.record_gid(session, user, payload[1])
-            await onboard_select_variant(callback.from_user.id, callback.message.message_id)
+            await onboarding.select_variant(callback.from_user.id, callback.message.message_id)
         if action == "variantselected":
             if len(payload) > 1:
                 await dbmanager.record_vid(session, user, payload[1])
@@ -81,51 +81,6 @@ async def dashboard(tid, mid=0):
         await bot.edit_message_text(chat_id=tid, message_id=mid, text="Welcome onboard")
     else:
         await bot.send_message(chat_id=tid, text="Welcome onboard")
-
-
-
-
-# http://kispython.ru/api/v1/variant/list
-async def onboard_select_variant(tid, mid=0):
-    r = requests.get('http://kispython.ru/api/v1/variant/list')
-    keyboard = types.InlineKeyboardMarkup(3)
-    for variant in r.json():
-        keyboard.add(
-            types.InlineKeyboardButton(text=variant+1, callback_data="variantselected_" + str(variant))
-        )
-    keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="variantonboard"))
-    if mid != 0:
-        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your variant")
-    else:
-        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your variant")
-    return
-
-
-async def onboard_select_group(tid, prefix, mid=0):
-    r = requests.get('http://kispython.ru/api/v1/group/' + prefix)
-    keyboard = types.InlineKeyboardMarkup()
-    for group in r.json():
-        keyboard.add(
-            types.InlineKeyboardButton(text=group["title"], callback_data="variantonboard_" + str(group["id"])))
-    keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="prefixonboard"))
-    if mid != 0:
-        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your group")
-    else:
-        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your group")
-    return
-
-
-async def onboard_select_prefix(tid, mid=0):
-    r = requests.get('http://kispython.ru/api/v1/group/prefixes')
-    keyboard = types.InlineKeyboardMarkup()
-    for prefix in r.json()["prefixes"]:
-        keyboard.add(types.InlineKeyboardButton(text=prefix, callback_data=("grouponboard_" + prefix)))
-
-    if mid is not 0:
-        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your prefix")
-    else:
-        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your prefix")
-    return
 
 
 if __name__ == '__main__':
---
+++ onboarding.py
@@ -0,0 +1,59 @@
+#              _                         _ _
+#             | |                       | (_)
+#   ___  _ __ | |__   ___   __ _ _ __ __| |_ _ __   __ _
+#  / _ \| '_ \| '_ \ / _ \ / _` | '__/ _` | | '_ \ / _` |
+# | (_) | | | | |_) | (_) | (_| | | | (_| | | | | | (_| |
+#  \___/|_| |_|_.__/ \___/ \__,_|_|  \__,_|_|_| |_|\__, |
+#                                                   __/ |
+#                                                  |___/
+
+#
+# Cute and user-friendly registration described here
+#
+
+import requests
+from aiogram import types
+
+from main import bot
+
+
+# Send message with variant list
+async def select_variant(tid, mid=0):
+    r = requests.get('http://kispython.ru/api/v1/variant/list')
+    keyboard = types.InlineKeyboardMarkup(3)
+    for variant in r.json():
+        keyboard.add(
+            types.InlineKeyboardButton(text=variant+1, callback_data="variantselected_" + str(variant))
+        )
+    keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="variantonboard"))
+    if mid != 0:
+        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your variant")
+    else:
+        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your variant")
+
+
+# Send message with group list
+async def select_group(tid, prefix, mid=0):
+    r = requests.get('http://kispython.ru/api/v1/group/' + prefix)
+    keyboard = types.InlineKeyboardMarkup()
+    for group in r.json():
+        keyboard.add(
+            types.InlineKeyboardButton(text=group["title"], callback_data="variantonboard_" + str(group["id"])))
+    keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="prefixonboard"))
+    if mid != 0:
+        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your group")
+    else:
+        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your group")
+
+
+# Send message with prefix list
+async def select_prefix(tid, mid=0):
+    r = requests.get('http://kispython.ru/api/v1/group/prefixes')
+    keyboard = types.InlineKeyboardMarkup()
+    for prefix in r.json()["prefixes"]:
+        keyboard.add(types.InlineKeyboardButton(text=prefix, callback_data=("grouponboard_" + prefix)))
+
+    if mid is not 0:
+        await bot.edit_message_text(chat_id=tid, message_id=mid, reply_markup=keyboard, text="Select your prefix")
+    else:
+        await bot.send_message(chat_id=tid, reply_markup=keyboard, text="Select your prefix")
