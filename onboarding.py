5f08b185b13605be722cd4107df89828a3fd9179
aaplnv <56654805+aaplnv@users.noreply.github.com>
2022-03-19 02:19:33 +0300

    [NEW] Beautiful labels

--- onboarding.py
+++ onboarding.py
@@ -29,7 +29,7 @@ async def select_variant(tid, mid=0):
             types.InlineKeyboardButton(text=variant+1, callback_data="variantselected_" + str(variant))
         )
     keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="variantonboard"))
-    await messenger.edit_or_send(tid, "Select your variant", keyboard, mid)
+    await messenger.edit_or_send(tid, "🗂️ Выберите свой вариант", keyboard, mid)
 
 
 # Send message with group list
@@ -40,7 +40,7 @@ async def select_group(tid, prefix, mid=0):
         keyboard.add(
             types.InlineKeyboardButton(text=group["title"], callback_data="variantonboard_" + str(group["id"])))
     keyboard.add(types.InlineKeyboardButton(text="<--", callback_data="prefixonboard"))
-    await messenger.edit_or_send(tid, "Select your group", keyboard, mid)
+    await messenger.edit_or_send(tid, "🤔 Выберите свою группу", keyboard, mid)
 
 
 # Send message with prefix list
@@ -48,5 +48,5 @@ async def select_prefix(tid, mid=0):
     r = requests.get(config['URL'] + 'group/prefixes')
     keyboard = types.InlineKeyboardMarkup()
     for prefix in r.json()["prefixes"]:
-        keyboard.add(types.InlineKeyboardButton(text=prefix, callback_data=("grouponboard_" + prefix)))
-    await messenger.edit_or_send(tid, "Select your prefix", keyboard, mid)
+        keyboard.add(types.InlineKeyboardButton(text=prefix + "  ➡️", callback_data=("grouponboard_" + prefix)))
+    await messenger.edit_or_send(tid, "🤔 Выберите свою группу", keyboard, mid)
