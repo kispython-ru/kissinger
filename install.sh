75b51d0cfd4178ae4e26b93b135b60c5f9b18d74
Aleksey Poluyanov <56654805+aaplnv@users.noreply.github.com>
2022-04-08 17:19:45 +0300

    Oh, forgot about pip

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.10 -y
 apt install python3.10-dev -y
 apt install python3.10-venv -y
+apt install python3-pip -y
 pip3 install poetry
 poetry install
 poetry shell
